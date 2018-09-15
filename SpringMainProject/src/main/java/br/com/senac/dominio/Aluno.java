0c746af35ee397e278d3a50ad612955b0db42dc0
claudiohp <claudio.henriquepj@gmail.com>
2018-09-15 19:25:20 -0300

    Projeto Struc

---
+++ SpringMainProject/src/main/java/br/com/senac/dominio/Aluno.java
@@ -0,0 +1,15 @@
+package br.com.senac.dominio;
+
+import java.io.Serializable;
+
+public class Aluno implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+}
---
+++ SpringMainProject/src/main/java/com/example/demo/SpringMainProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMainProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMainProjectApplication.class, args);
+	}
+}
